c83cba1acb7b2423251e80b6754aa4cbdcab0f0b
jgustavoj <gjime011@fiu.edu>
2020-11-13 01:14:00 +0000

    database started

--- src/models.py
+++ src/models.py
@@ -2,7 +2,48 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
+
+
 class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(250), unique=False, nullable=False)
+    last_name = db.Column(db.String(250), unique=False, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    phone_number = db.Column(db.int(250), unique=False, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "phone_number": self.phone_number,
+            "password": self.password,
+            "is_active": self.is_active,
+            # do not serialize the password, its a security breach
+        }
+
+class Events(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
