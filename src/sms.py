e274aed67600a428985c4e40c14e1dd3bdf6f1f0
jgustavoj <gjime011@fiu.edu>
2020-11-26 18:06:49 +0000

    changed token for security

--- src/sms.py
+++ src/sms.py
@@ -4,8 +4,8 @@ import os
 def send(body='', to='+13057984105'):
     # Your Account Sid and Auth Token from twilio.com/console
     # DANGER! This is insecure. See http://twil.io/secure
-    account_sid = 'AC94e8b0d3e7dd7969485fbc45b982abda'
-    auth_token = 'c24266bc45f773b51045975e7e34d643'
+    # account_sid = 'account from twilio'
+    # auth_token = 'token from twilio account'
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
@@ -16,3 +16,4 @@ def send(body='', to='+13057984105'):
 
     print(message.sid)
 
+# make sure to add account sid and auth token from twilio personal account on line 7 and 8
